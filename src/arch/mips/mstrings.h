6c5664bc3bcbabb45514fca38fcf2c85a1844b31
Mithreindeir <mithreindeir@gmail.com>
2018-05-14 14:18:51 -0400

    Adds missing files

---
+++ src/arch/mips/mstrings.h
@@ -0,0 +1,6 @@
+#ifndef MIPS_STRINGS_H
+#define MIPS_STRINGS_H
+
+extern const char * mips_registers[32];
+
+#endif
---
+++ src/common/type.h
@@ -0,0 +1,10 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+#include <stdint.h>
+
+typedef unsigned char u8;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#endif
