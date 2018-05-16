85b9e20039c8bfbd046b103defae799dd21b4c5e
Mithreindeir <mithreindeir@gmail.com>
2018-05-16 13:19:19 -0400

    Removes unused/dev files from code coverage

--- src/main.c
+++ src/main.c
@@ -2,17 +2,29 @@
 
 int main()
 {
-	
+
   struct disassembler *ds = ds_init(X86_ARCH, MODE_64B);
   unsigned char bytes[] =  "\x55\x48\x89\xe5\xb8\x00\x00\x00\x00\xc3";
 
   ds_decode(ds, bytes, sizeof(bytes)-1, 0x0);
   struct dis *dis = NULL;
-  
+
   DS_FOREACH(ds, dis) {
     printf("%#08lx:\t%s\t%s\n", dis->address, dis->mnemonic, dis->op_squash);
   }
-  
+
+  ds_destroy(ds);
+  struct disassembler *ds = ds_init(MIPS_ARCH, MODE_64B);
+  unsigned char bytes[] =  "\x55\x48\x89\xe5\xb8\x00\x00\x00\x00\xc3";
+
+  ds_decode(ds, bytes, sizeof(bytes)-1, 0x0);
+  struct dis *dis = NULL;
+
+  DS_FOREACH(ds, dis) {
+    printf("%#08lx:\t%s\t%s\n", dis->address, dis->mnemonic, dis->op_squash);
+  }
+
+  ds_destroy(ds);
   ds_destroy(ds);
   return 0;
 }
